e9559d89335aa25cc8e88b308bd9126b1de62ab5
eileenpaula <epaul037@fiu.edu>
2023-06-29 17:20:42 +0000

    First working prototype!

--- Development-log.md
+++ Development-log.md
@@ -3,3 +3,9 @@
 - added request to PokeAPI
 - ran "pip install openai"
 - added prompt for the user to enter a Pokemon's name and error handling associated
+
+* June 28, 2023
+- imported addtional packages (flask)
+- began integration of website
+- fully working prototype functioning
+- added unit testing and check-style doc
--- pokeapi-artwork.py
+++ pokeapi-artwork.py
@@ -1,30 +1,43 @@
-from flask import Flask as fl
+from flask import Flask, redirect, render_template, request, url_for
 import requests
 import openai
 import json
 import os
+import openai
 
 
-url = "https://pokeapi.co/api/v2/pokemon/"
-error = "Invalid Pokemon name!"
-valid_name = False
+def generate_art():
+    openai.api_key = "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"
+    url = "https://pokeapi.co/api/v2/pokemon/"
+    error = "Invalid Pokemon name!"
 
-print("Generate artwork based off of a Pokemon of your choosing!")
-print("Enter the name of a pokemon or enter exit to close the program.")
-while valid_name is False:
-    pokemon_name = input("Please enter the name of a Pokemon in all lowercase: ")
-    if len(pokemon_name) == 0:
-        print(error + " Name cannot be empty.")
-        continue
-    elif pokemon_name == "exit":
-        break
-    try:
-        response = requests.get(url + pokemon_name).json()
-    except:
-        print(error + " Pokemon " + pokemon_name + " not found.")
-    else:
-        valid_name = True
+    valid_name = False
+    print("Generate pokemon art using openAI's API.")
+    while valid_name is False:
+        pokemon_name = input("Please enter the name of a Pokemon in all lowercase: ")
+        if len(pokemon_name) == 0:
+            print(error + " Name cannot be empty.")
+            continue
+        elif pokemon_name == "exit":
+            break
+        try:
+            response = requests.get(url + pokemon_name).json()
+        except:
+            print(error + " Pokemon " + pokemon_name + " not found." )
+        else:             
+            valid_name = True 
+    
+    desc = "Generate a fully colored, realistic art piece with a colorful background based on the following:\n\nPokemon: " + pokemon_name + "\nDescription:\n"
+    desc += requests.get(response["species"]["url"]).json()["flavor_text_entries"][0]["flavor_text"]
 
+    response = openai.Image.create(
+    prompt=desc,
+    n=1,
+    size="1024x1024"
+    )
+    image_url = response['data'][0]['url']
 
+    print("The following URL contains the generated artwork:\n")
+    return image_url
 
-print(response)
+print(generate_art())
