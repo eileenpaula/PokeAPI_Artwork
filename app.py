7af81e52065251396d6d4dc158cb385e7b9cc838
eileenpaula <epaul037@fiu.edu>
2023-07-10 17:39:30 -0400

    Configured website using Flask

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")                          # this tells you the URL the method below is related to
-def hello_world():
-    return "<p>Hello, World!</p>"        # this prints HTML to the webpage
-  
-if __name__ == '__main__':               # this should always be at the end
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/prevAW')
+def prevAW():
+    return render_template('/prevAW.html')
+
+if __name__ == '__main__':            
     app.run(debug=True, host="0.0.0.0")
-    
