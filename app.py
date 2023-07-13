db96d941118d291eb281591ea8bf8af61ce08675
eileenpaula <epaul037@fiu.edu>
2023-07-13 13:40:00 -0400

    Database works

--- app.py
+++ app.py
@@ -2,44 +2,55 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
 
+import sqlalchemy as db
+
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
+# Creating Database and Table
+engine = db.create_engine('sqlite:///data_base_name.sqlite') #Create data_base_name.sqlite automatically
+connection = engine.connect()
+metadata = db.MetaData()
 
-#engine = db.create_engine('sqlite:///data_base_name.db')
+# Json.dumps(arr)
+emp = db.Table('emp', metadata,
+              db.Column('pokemon', db.String()),
+              db.Column('url', db.String(), nullable=False)
+              )
+
+metadata.create_all(engine) #Creates the table
+
+print("table created")
 
 @app.route('/', methods=['GET', 'POST'])
-#@app.route('/index', methods=['GET', 'POST'])
-def index():
-    print("THIS METHOD IS ", request.method)
-    if request.method == "POST":
-        print("THIS IS A POST REQUEST")
-        print(request.values)
+def index():    
+    print(request.form)
+    if request.method == 'POST':
         poke_search = request.form.get('poke-input')
-        print(request.form)
+        with engine.connect() as connection:
+            data = {"pokemon":"this works", "url":"N/A"}
+            connection.execute(emp.insert(), data)
+
+    
+    print(request.form)
     return render_template('index.html')
 
+@app.route('/testing', methods=['GET', 'POST'])
+def testing(): 
+    with engine.connect() as connection:
+        query = db.select(emp)
+        query_result = connection.execute(query)
+        rows = query_result.fetchall()
+        print(rows)
+    return "Hello World!"
+
+'''
 @app.route('/prevAW')
 def prevAW():
-    
-    #with engine.connect() as connection:
-        #query_result = connection.execute(db.text("SELECT pokemon, urls FROM table_name;")).fetchall()
-        #return render_template('prevAW.html', results=query_result)
-
     engine = db.create_engine('sqlite:///data_base_name.db')
     connection = engine.connect()
-
     query_result = connection.execute(db.text("SELECT pokemon, urls FROM table_name;")).fetchall()
-
     df = pd.DataFrame(query_result, columns=['pokemon', 'urls'])
-
-    return render_template('prevAW.html', data=df.to_dict(orient='records'))
-    #return render_template('/prevAW.html')
-'''
-class Images(db.Model):
-    pokemon_name = db.Column(db.String, nullable=False)
-    image_url = db.Column(db.String, nullable=False)'''
+    return render_template('prevAW.html', data=df.to_dict(orient='records'))'''
 
 if __name__ == '__main__':            
     app.run(debug=True, host="0.0.0.0")
